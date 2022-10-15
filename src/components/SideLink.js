70957779020206bdce4721a8446df49c26cb5e64
Eren <erenerol8@gmail.com>
2022-10-15 18:19:39 +0300

    sidelink

---
+++ src/components/SideLink.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  ProfileIcon,
+  ExploreIcon,
+  HomeIcon,
+  MoreIcon,
+  MessagesIcon,
+  NotificationIcon,
+  ListsIcon,
+  BookmarksIcon,
+} from "../icon/Icon";
+
+const sideLinks = [
+  {
+    name: "Home",
+    icon: HomeIcon,
+  },
+  {
+    name: "Explore",
+    icon: ExploreIcon,
+  },
+  {
+    name: "Notifications",
+    icon: NotificationIcon,
+  },
+  {
+    name: "Messages",
+    icon: MessagesIcon,
+  },
+  {
+    name: "Bookmarks",
+    icon: BookmarksIcon,
+  },
+  {
+    name: "Lists",
+    icon: ListsIcon,
+  },
+  {
+    name: "Profile",
+    icon: ProfileIcon,
+  },
+  {
+    name: "More",
+    icon: MoreIcon,
+  },
+];
+
+const SideLink = () => {
+  return <div></div>;
+};
+export default SideLink;
--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import twitterIcon from "../images/icon-twitter.svg";
+
 import {
   ProfileIcon,
   ExploreIcon,
@@ -12,16 +14,24 @@ import {
 
 const Siderbar = () => {
   return (
-    <div className="w-72 bg-blue-200 ">
-      <h1>Sidebar</h1>
-      <HomeIcon />
-      <ExploreIcon />
-      <NotificationIcon />
-      <MessagesIcon />
-      <ProfileIcon />
-      <MoreIcon />
-      <ListsIcon />
-      <BookmarksIcon />
+    <div className="flex flex-col justify-between w-72 px-2">
+      <div>
+        <div className="mt-1  mb-4 ml-l flex items-center justify-center w-12 h-12 rounded-full hover:bg-gray-lightest transform transition-colors duration-200">
+          <img src={twitterIcon} alt="Twitter Logo" className="w-9 h-9"></img>
+        </div>
+        <nav className="mb-4">
+          <ul>
+            <li>Home</li>
+            <li>Explore</li>
+          </ul>
+        </nav>
+        <nav>
+          <button className="bg-primary-base hover:bg-primary-dark text-white shadow-lg rounded-full py-3 px-8 w-11/12 transform transition-colors duration-200">
+            Tweet
+          </button>
+        </nav>
+      </div>
+      <div>Alt taraf</div>
     </div>
   );
 };
