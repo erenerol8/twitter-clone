b1390c6ac0ef4d206337df17b9b4a62724bd92cc
Eren <erenerol8@gmail.com>
2022-10-15 21:00:59 +0300

    commit

--- src/components/SideLink.js
+++ src/components/SideLink.js
@@ -10,41 +10,6 @@ import {
   BookmarksIcon,
 } from "../icon/Icon";
 
-const sideLinks = [
-  {
-    name: "Home",
-    icon: HomeIcon,
-  },
-  {
-    name: "Explore",
-    icon: ExploreIcon,
-  },
-  {
-    name: "Notifications",
-    icon: NotificationIcon,
-  },
-  {
-    name: "Messages",
-    icon: MessagesIcon,
-  },
-  {
-    name: "Bookmarks",
-    icon: BookmarksIcon,
-  },
-  {
-    name: "Lists",
-    icon: ListsIcon,
-  },
-  {
-    name: "Profile",
-    icon: ProfileIcon,
-  },
-  {
-    name: "More",
-    icon: MoreIcon,
-  },
-];
-
 const SideLink = () => {
   return <div></div>;
 };
